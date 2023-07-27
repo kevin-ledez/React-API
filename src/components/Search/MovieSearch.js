162213ab08fe7d5fc3ee555ff601cf4922c62212
kevin-ledez <kevin.ledez@gmail.com>
2023-07-28 00:41:39 +0200

    👌 IMPROVE: Input Search

--- src/components/Search/MovieSearch.js
+++ src/components/Search/MovieSearch.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./MovieSearch.css";
 
 const MovieSearch = () => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -7,19 +8,18 @@ const MovieSearch = () => {
   const API_KEY = "005b56149e8ba5468ce2c4974c5d4cb9";
   const BASE_URL = "https://api.themoviedb.org/3";
 
-  const handleSearch = async (e) => {
-    e.preventDefault();
+  const handleSearch = async (query) => {
     try {
       // Recherche des films associés à l'acteur
       const actorResponse = await fetch(
-        `${BASE_URL}/search/person?api_key=${API_KEY}&query=${searchQuery}`
+        `${BASE_URL}/search/person?api_key=${API_KEY}&query=${query}`
       );
       const actorData = await actorResponse.json();
       const actorId = actorData.results[0]?.id || null;
 
       // Recherche des films avec le mot clé dans le titre
       const movieResponse = await fetch(
-        `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${searchQuery}`
+        `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`
       );
       const movieData = await movieResponse.json();
 
@@ -46,28 +46,35 @@ const MovieSearch = () => {
     }
   };
 
+  const handleChange = (e) => {
+    const query = e.target.value;
+    setSearchQuery(query);
+
+    // Appeler handleSearch pour effectuer la recherche automatiquement
+    handleSearch(query);
+  };
+
   return (
-    <div>
-      <form onSubmit={handleSearch}>
-        <input
-          type="text"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          placeholder="Rechercher un acteur ou un mot clé dans le titre"
-        />
-        <button type="submit">Rechercher</button>
-      </form>
+    <div className="container">
+    <div className="search">
+      <input
+        className="search"
+        type="text"
+        value={searchQuery}
+        onChange={handleChange} // Appel de la fonction handleChange lorsqu'il y a un changement dans l'input
+        placeholder="Rechercher un acteur ou un mot clé dans le titre"
+      />
       <ul>
         {results && results.length > 0 ? (
           results.map((result) => (
+            //console.log(result),
             <li key={result.id}>
               <p>Titre : {result.title}</p>
             </li>
           ))
-        ) : (
-          <li>Aucun résultat trouvé.</li>
-        )}
+        ) : ('')}
       </ul>
+    </div>
     </div>
   );
 };
